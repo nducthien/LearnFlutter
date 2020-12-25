cb461f075307dc87f21b08d6d9ed5167d96c0ab9
nducthien <thien.chotrax23@gmail.com>
2020-12-25 15:34:36 +0700

    lear flutter crud

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ void main() {
   List<Car> cars = <Car>[]; // a blank list
   cars.add(Car(name: 'Lexus LX570', yearOfProduction: 2018));
   cars.add(Car(name: 'Lexus LX570 Supper Sport', yearOfProduction: 2021));
-  cars.add(Car(name: 'Mercedes-Mayback S-class saloon', yearOfProduction: 2020));
+  cars.add(Car(name: 'Mercedes-Maybach GLS 600 4MATIC', yearOfProduction: 2020));
   cars.add(Car(name: 'Mercedes S-class saloon', yearOfProduction: 2019));
+  cars.add(Car(name: 'Mercedes S-650', yearOfProduction: 2020));
 
-  // how to sort this list by year
+  // todo - how to sort this list by year
   // cars.sort((ca1, ca2) {
   //   return ca1.yearOfProduction - ca2.yearOfProduction;
   // });
@@ -18,15 +19,24 @@ void main() {
   //   return ca2.yearOfProduction - ca1.yearOfProduction;
   // });
 
-  // how to sort by name
-  cars.sort((ca1, ca2) {
-    return ca1.name.compareTo(ca2.name);
-  });
+  // todo how to sort by name
+  // cars.sort((ca1, ca2) {
+  //   return ca1.name.compareTo(ca2.name);
+  // });
 
-  //
+  // todo - how to update an item in the list ?
+  //cars[0].yearOfProduction = 2015;
+  
+  // todo -  how to filter ? ex: get only cars in 2020
+  var filterCar = cars.where((car) => car.yearOfProduction == 2020 && car.name.contains("Mercedes")).toList();
+
+  // todo delete an item = using filter
+  cars = cars.where((element) => element.name != 'Lexus LX570').toList();
+
+
   runApp(Center(
       child: Text(cars.toString(),
-          style: TextStyle(fontSize: 20),
-          textDirection: TextDirection.ltr // left to right - ctrl + shift + \
+          style: TextStyle(fontSize: 15),
+          textDirection: TextDirection.ltr // left to right
           )));
 }
